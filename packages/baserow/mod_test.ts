cfddd2fe8d709f1103004a2e8ae81cbebb15d29d
Yeechan Lu <git@orzfly.com>
2025-08-01 16:22:09 +0800

    test(baserow): use example host

--- packages/baserow/mod_test.ts
+++ packages/baserow/mod_test.ts
@@ -2,10 +2,10 @@ import { assertEquals } from "@std/assert";
 import { BaserowInstance } from "./mod.ts";
 
 Deno.test(function addTest() {
-  const instance = new BaserowInstance("https://api.baserow.io/api/database/v3/", {
+  const instance = new BaserowInstance("https://example.com/api/database/v3/", {
     Authorization: "Token 1234567890",
   });
 
-  assertEquals(instance.backend, "https://api.baserow.io/api/database/v3");
+  assertEquals(instance.backend, "https://example.com/api/database/v3");
   assertEquals(instance.header, { Authorization: "Token 1234567890" });
 });

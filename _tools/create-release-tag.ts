901e0494d149bf761247b8688e0db42623841928
Yeechan Lu <git@orzfly.com>
2025-08-01 18:27:34 +0800

    feat: create release tag script

---
+++ _tools/create-release-tag.ts
@@ -0,0 +1,19 @@
+import $ from "@david/dax"
+import * as fs from "node:fs/promises";
+
+const releaseFile = await fs.readFile("./Releases.md", "utf-8");
+const lines = releaseFile.split("\n");
+const latestReleaseIndex = lines.findIndex((x: string) => x.startsWith("### "))
+const latestRelease = lines[latestReleaseIndex]!.split(" ")[1];
+
+const nextReleaseOffset = lines.slice(latestReleaseIndex + 1).findIndex((x: string) => x.startsWith("### "))
+const releaseNotes = lines.slice(latestReleaseIndex + 1, nextReleaseOffset >= 0 ? nextReleaseOffset + latestReleaseIndex + 1 : undefined).join("\n").trim();
+
+console.log(`===========`);
+console.log(releaseNotes);
+console.log(`===========`);
+
+const tag = `release-${latestRelease}`;
+
+await $`git tag ${tag}`;
+await $`git push origin ${tag}`;

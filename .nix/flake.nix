1256ce476bb5ebf1abf5a1464985e75511928538
Yeechan Lu <git@orzfly.com>
2025-09-24 12:55:51 +0800

    chore: upgrade nix

--- .nix/flake.nix
+++ .nix/flake.nix
@@ -1,42 +1,48 @@
 {
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
     devshell.url = "github:numtide/devshell";
     devshell.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = inputs@{ self, ... }:
-    inputs.flake-utils.lib.eachDefaultSystem (system:
+  outputs =
+    inputs@{ self, ... }:
+    inputs.flake-utils.lib.eachDefaultSystem (
+      system:
       let
         pkgs = import inputs.nixpkgs {
           inherit system;
           overlays = [ inputs.devshell.overlays.default ];
         };
-        node = pkgs.nodejs_24;
-        yarn = pkgs.yarn-berry;
+        unstable-pkgs = import inputs.nixpkgs-unstable {
+          inherit system;
+        };
       in
       {
         devShell = pkgs.devshell.mkShell {
-          imports = [{
-            name = "devshell";
-            packages = [
-              node
-              yarn
-              pkgs.deno
-            ];
+          imports = [
+            {
+              name = "devshell";
+              packages = [
+                pkgs.nixd
+                pkgs.nixfmt-rfc-style
+                unstable-pkgs.deno
+              ];
 
-            env = [
-              {
-                name = "PATH";
-                prefix = "node_modules/.bin";
-              }
-            ];
+              env = [
+                {
+                  name = "PATH";
+                  prefix = "node_modules/.bin";
+                }
+              ];
 
-            commands = [
-            ];
-          }];
+              commands = [
+              ];
+            }
+          ];
         };
-      });
+      }
+    );
 }
-
